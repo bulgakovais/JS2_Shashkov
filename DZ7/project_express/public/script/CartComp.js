5ef425aea2ef0dada9921c5a1f70d5eef6d6e6f7
Irina <irina2358@yandex.ru>
2021-08-16 18:08:17 +0300

    DZ7

---
+++ DZ7/project_express/public/script/CartComp.js
@@ -0,0 +1,81 @@
+Vue.component('cart', {
+    data() {
+        return {
+            cartUrl: '/getBasket.json',
+            cartItems: [],
+            showCart: false,
+        }
+    },
+    methods: {
+        addProduct(product) {
+            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            if (find) {
+                this.$parent.putJson(`/api/cart/${find.id_product}`, { quantity: 1 })
+                find.quantity++
+            } else {
+                let prod = Object.assign({ quantity: 1 }, product)
+                this.$parent.postJson('/api/cart', prod)
+                    .then(data => {
+                        if (data.result === 1) {
+                            this.cartItems.push(prod)
+                        }
+                    })
+            }
+        },
+        remove(item) {
+            this.$parent.deleteJson(`/api/cart`, item)
+                .then(data => {
+                    if (data.result === 1) {
+                        if (item.quantity > 1) {
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1)
+                        }
+                    }
+                })
+        },
+    },
+    mounted() {
+        this.$parent.getJson(`/api/cart`)
+            .then(data => {
+                for (let el of data.contents) {
+                    this.cartItems.push(el);
+                }
+            });
+    },
+    template: `
+    <div>
+    
+    <button class="button-cart button textColor" type="button" @click="showCart = !showCart">
+    <i class="fas fa-shopping-cart"></i>
+    </button>
+
+    <div class="cart-block" v-show="showCart">
+        <p v-if="!cartItems.length" class="textColor">Ваша корзина совсем пуста.. :(</p>
+        <cart-item class ="cart-item" v-for="item of cartItems" :cart-item = "item" :key="item.id_product"></cart-item>
+    </div>
+    </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem'],
+    template: `
+    <div class="cart-item">
+         <div class="cart_product">
+            <img class="image-cart-product" src="image/shirt.jpg" alt="image">
+            <div class="description">
+                <p class="product-title paragraf">{{cartItem.product_name}}</p>
+                <p class="product-quantity paragraf">Кол-во: {{cartItem.quantity}}</p>
+                <p class="product-single-price paragraf">{{cartItem.price}}&nbspp.</p>
+            </div>
+            <div class="right-block">
+            <p class="product-price">Итого: {{cartItem.quantity * cartItem.price}}&nbspp.</p>
+            <button class="delete-button" @click="$parent.remove(cartItem)">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+        </div>
+
+        </div>
+    </div>
+    `
+})
---
+++ DZ7/project_express/public/script/ProductComp.js
@@ -0,0 +1,45 @@
+Vue.component('products', {
+    data() {
+        return {
+            catalogUrl: '',
+            products: [],
+            filtered: [],
+        }
+    },
+    methods: {
+        filter(userSearch) {
+            let regexp = new RegExp(userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    mounted() {
+        this.$parent.getJson(`/api/products`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+    },
+    template: `
+         <div class="products">
+            <p v-if="!filtered.length" class="textColor textSize">Нет данных. И товаров нет.. Ээх...</p>
+             <product v-for="item of filtered" :key="item.id_product" :product="item"></product>
+        </div>
+    `
+});
+
+Vue.component('product', {
+    props: ['product'],
+
+    template: `
+    <div class="product-item">
+    <img class="productImage" src="image/shirt.jpg" alt="image">
+        <div class="description">
+            <h3 class="textColor">{{product.product_name}}</h3>
+            <p class="productPrice">{{product.price}} ₽</p>
+        </div>
+        <button @click="$root.$refs.cart.addProduct(product)" class="addToCart">Добавить в корзину</button>
+    </div>
+    `
+})
